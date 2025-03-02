5c819858d5ac1c2a1ea8e616b510f3d087f9f579
unknown <jackhasakeyboard@gmailcom>
2025-03-02 03:13:22 -0700

    Moved declarations to top level

--- public/script/index.js
+++ public/script/index.js
@@ -1,33 +1,33 @@
+var vertexShaderText = 
+[
+'precision mediump float;',
+'',
+'attribute vec3 vertPosition;',
+'attribute vec3 vertColor;',
+'varying vec3 fragColor;',
+'uniform mat4 mWorld;',
+'uniform mat4 mView;',
+'uniform mat4 mProj;',
+'',
+'void main()',
+'{',
+'  fragColor = vertColor;',
+'  gl_Position = mProj * mView * mWorld * vec4(vertPosition, 1.0);',
+'}'
+].join('\n');
+
+var fragmentShaderText =
+[
+'precision mediump float;',
+'',
+'varying vec3 fragColor;',
+'void main()',
+'{',
+'  gl_FragColor = vec4(fragColor, 1.0);',
+'}'
+].join('\n');
+
 document.addEventListener("DOMContentLoaded", async function() {
-	var vertexShaderText = 
-	[
-	'precision mediump float;',
-	'',
-	'attribute vec3 vertPosition;',
-	'attribute vec3 vertColor;',
-	'varying vec3 fragColor;',
-	'uniform mat4 mWorld;',
-	'uniform mat4 mView;',
-	'uniform mat4 mProj;',
-	'',
-	'void main()',
-	'{',
-	'  fragColor = vertColor;',
-	'  gl_Position = mProj * mView * mWorld * vec4(vertPosition, 1.0);',
-	'}'
-	].join('\n');
-
-	var fragmentShaderText =
-	[
-	'precision mediump float;',
-	'',
-	'varying vec3 fragColor;',
-	'void main()',
-	'{',
-	'  gl_FragColor = vec4(fragColor, 1.0);',
-	'}'
-	].join('\n');
-
 	console.log('This is working');
 
 	var canvas = document.getElementById('disp');
