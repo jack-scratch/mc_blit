0d8294bed90aec29050a560c4c3f630dd5ab6299
unknown <jackhasakeyboard@gmailcom>
2025-03-30 13:44:33 -0600

    Continually distributed bricks away for every layer upwards

--- public/script/index.js
+++ public/script/index.js
@@ -26,14 +26,14 @@ document.addEventListener("DOMContentLoaded", async function() {
 	const red = [1.0, 0.0, 0.0];
 
 	for (let y = 0; y < 3; y++) {
-		set.push(new Brick(red, [0.0, y * strideT, 0.0]));
+		set.push(new Brick(red, [y * strideS, y * strideT, 0.0]));
 
 		for (let x = 1; x < 5; x++) {
-			set.push(new Brick(red, [x * strideS, y * strideT, -x * strideS]));
+			set.push(new Brick(red, [(x * strideS) + (y * strideS), y * strideT, -x * strideS]));
 		}
 
 		for (let x = -1; x > -5; x--) {
-			set.push(new Brick(red, [x * strideS, y * strideT, -x * strideS]));
+			set.push(new Brick(red, [(x * strideS) + (y * strideS), y * strideT, -x * strideS]));
 		}
 	}
 
