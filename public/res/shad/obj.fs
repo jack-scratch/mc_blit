76287ac5c9d02e51798258b80c124db00276cd56
unknown <jackhasakeyboard@gmailcom>
2025-03-30 12:38:25 -0600

    Implemented directional lighting

--- public/res/shad/obj.fs
+++ public/res/shad/obj.fs
@@ -5,6 +5,14 @@ precision mediump float;
 in vec3 _pos;
 out vec4 frag;
 
+vec3 sun = vec3(1.0, 0.0, -1.0);
+
+vec3 col = vec3(1.0, 0.0, 0.0);
+
 void main() {
-	frag = vec4(_pos, 1.0);
+	vec3 normFace = normalize(cross(dFdx(_pos), dFdy(_pos)));
+
+	float diff = max(dot(normFace, normalize(sun)), 0.0) * 0.2;
+
+	frag = vec4((1.0 - diff) * col, 1.0);
 }
--- public/res/shad/obj.vs
+++ public/res/shad/obj.vs
@@ -3,12 +3,11 @@
 precision mediump float;
 
 in vec3 pos;
+out vec3 _pos;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 proj;
-
-out vec3 _pos;
 
 void main() {
 	gl_Position = proj * view * model * vec4(pos, 1.0);
