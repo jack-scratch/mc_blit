06b79371c1696c7a7d4aac1bc2142d686d34d23c
unknown <jackhasakeyboard@gmailcom>
2025-03-30 11:05:06 -0600

    Updated camera position

--- public/script/index.js
+++ public/script/index.js
@@ -158,7 +158,7 @@ document.addEventListener("DOMContentLoaded", async function() {
 	let view = new Float32Array(16);
 	let proj = new Float32Array(16);
 	mat4.identity(world);
-	mat4.lookAt(view, [0, 0, -8], [0, 0, 0], [0, 1, 0]);
+	mat4.lookAt(view, [-10, 10, -10], [0, 0, 0], [0, 1, 0]);
 	mat4.perspective(proj, glMatrix.toRadian(45), canvas.clientWidth / canvas.clientHeight, 0.1, 1000.0);
 
 	gl.uniformMatrix4fv(uniWorld, gl.FALSE, world);
